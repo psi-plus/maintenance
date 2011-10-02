d70dee47e7750657c7c80e015ec713f609113adf
zet <vladimir.shelukhin@gmail.com>
2011-10-02 15:01:56 +0400

    * psiplus-install.nsi, upload-installer-to-googlecode.cmd (0.15.5122)

--- scripts/win32/upload-installer-to-googlecode.cmd
+++ scripts/win32/upload-installer-to-googlecode.cmd
@@ -1,5 +1,5 @@
 @echo off
 @echo Uploading Psi+ Installer to Google Code
-call autobuild\googlecode_upload.py -p "psi-dev" -s "Psi+ Windows Installer || psi-git 11.09.2011 17:21 MSD || Qt 4.7.4 || Win32 OpenSSL Libs v0.9.8r || Psimedia/GStreamer included" -l "Featured,Windows,Installer" "setup\psi-plus-0.15.5116-win32-setup.exe"
+call autobuild\googlecode_upload.py -p "psi-dev" -s "Psi+ Windows Installer || psi-git 02.10.2011 14:59 MSD || Qt 4.7.4 || Win32 OpenSSL Libs v1.0.0e || Psimedia/GStreamer included" -l "Featured,Windows,Installer" "setup\psi-plus-0.15.5122-win32-setup.exe"
 @echo Completed
 pause & pause
