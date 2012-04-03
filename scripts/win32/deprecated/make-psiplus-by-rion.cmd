44c42f93c7ecf54b4561cef153a7908d8dac1530
rion <rion@skydns.ru>
2012-04-04 01:26:05 +0600

    Improved make-psiplus-by-rion.cmd

--- scripts/win32/deprecated/make-psiplus-by-rion.cmd
+++ scripts/win32/deprecated/make-psiplus-by-rion.cmd
@@ -1,15 +1,15 @@
-@echo off
+::@echo off
 setlocal
 
-set PSI_GIT=git://git.psi-im.org/psi.git
+set PSI_GIT=git://github.com/psi-im/psi.git
 set PSI_PLUS_MAIN_GIT=git://github.com/psi-plus/main.git
-set PSIPLUS_SVN=http://psi-dev.googlecode.com/svn/trunk
+set QCONF_SVN=http://delta.affinix.com/svn/trunk/qconf/
 set ICONSETS=system clients activities moods affiliations
 
 @echo Check enviroment
 set ORIGCD=%CD%
 set ERRORLEVEL=0
-if defined GITDIR goto :findSVN
+if defined GITDIR goto :checkUtils
 call :getInstallDirFromRegistryWOW64 Git_is1
 set GITDIR=%InstallationLocation%bin\
 
@@ -23,7 +23,7 @@ set SED=%GITDIR%sed.exe
 set PATCH=%GITDIR%patch.exe
 ::set SVN=%SVNDIR%svn.exe
 ::set SVNVERSION=%SVNDIR%svnversion.exe
-set WORKDIR=%CD%\psi
+set WORKDIR=%CD%
 
 if defined MINGWDIR goto :ensureExist
 call :getInstallDirFromRegistryWOW64 "Qt SDK" HKCU
@@ -43,16 +43,16 @@ if not exist "%SED%" @echo sed.exe not found. Please set GITDIR before start&got
 if not exist "%WORKDIR%" mkdir "%WORKDIR%"
 cd "%WORKDIR%"
 @echo Fetching git sources
-if not exist git "%GIT%" clone %PSI_GIT% git
+if not exist git "%GIT%" clone %PSI_GIT% git || @echo "failed to fetch Psi repo"&goto :failExit
 cd git
-"%GIT%" pull
-"%GIT%" submodule update --init || @echo "failed to fetch Psi repo"&goto :failExit
+"%GIT%" pull || @echo "failed to update Psi repo"&goto :failExit
+"%GIT%" submodule update --init || @echo "failed to fetch Psi submodules"&goto :failExit
 
 cd "%WORKDIR%"
 @echo Fetching Psi+ main repo
-if not exist git-plus "%GIT%" clone %PSI_PLUS_MAIN_GIT% git-plus
-cd git-plus
-"%GIT%" pull
+if not exist main "%GIT%" clone %PSI_PLUS_MAIN_GIT% main || @echo "failed to clone Psi+ main repo"&goto :failExit
+cd main
+"%GIT%" pull || @echo "failed to update Psi+ main repo"&goto :failExit
 
 @echo Prepare sources
 if exist "%WORKDIR%\build" rd /S /Q "%WORKDIR%\build" || @echo "failed to remove old build dir"&goto :failExit
@@ -64,16 +64,16 @@ cd "%WORKDIR%\git\iris"
 "%GIT%" checkout-index -a --f --prefix=%WORKDIR%\build\iris\
 cd "%WORKDIR%\git\src\libpsi"
 "%GIT%" checkout-index -a --f --prefix=%WORKDIR%\build\src\libpsi\
-xcopy /Y /E /Q "%WORKDIR%\git-plus\iconsets" "%WORKDIR%\build\iconsets" || @echo icons export failed&goto :failExit
-copy /Y "%WORKDIR%\git-plus\app.ico" "%WORKDIR%\build\win32\app.ico"
+xcopy /Y /E /Q "%WORKDIR%\main\iconsets" "%WORKDIR%\build\iconsets" || @echo icons export failed&goto :failExit
+copy /Y "%WORKDIR%\main\app.ico" "%WORKDIR%\build\win32\app.ico"
 cd "%WORKDIR%"\build
-FOR /F "usebackq delims==" %%i IN (`dir /B "%WORKDIR%\git-plus\patches\*.diff"`) DO (
+FOR /F "usebackq delims==" %%i IN (`dir /B "%WORKDIR%\main\patches\*.diff"`) DO (
 	@echo Apply: %%i
 	@echo %%i > "%WORKDIR%\patch.log"
-	"%PATCH%" -p1 <  "%WORKDIR%\git-plus\patches\%%i" >> "%WORKDIR%\patch.log" || @echo patch failed&goto :failExit
+	"%PATCH%" -p1 <  "%WORKDIR%\main\patches\%%i" >> "%WORKDIR%\patch.log" || @echo patch failed&goto :failExit
 )
 
-cd "%WORKDIR%"\git-plus
+cd "%WORKDIR%"\main
 for /f "tokens=2 delims=-" %%i in ('"%GIT%" describe --tags') do set psiplusrev=%%i
 set /a psiplusrev=%psiplusrev%+5000
 call :doSed "s/\(xxx\)/%psiplusrev%/" "%WORKDIR%\build\src\applicationinfo.cpp"
@@ -82,16 +82,34 @@ if %ERRORLEVEL% neq 0 @echo failed to set revision&goto :failExit
 cd "%WORKDIR%\build"
 :: TODO qconf, configure and make here 
 
+set QCONF="%WORKDIR%"\qconf\qconf.exe
+if exist "%QCONF%" goto :callQconf
+cd "%WORKDIR%"
+"%GIT%" svn clone -r693:HEAD %QCONF_SVN% qconf || @echo failed to clone qconf&goto :failExit
+@echo compile qconf
+cd "%WORKDIR%"\qconf
+configure.exe || @echo failed to configure qconf&goto :failExit
+mingw32-make || @echo failed to make qconf&goto :failExit
+
+
+:callQconf
+cd "%WORKDIR%"\build
+"%QCONF%" || @echo qconf call failed&goto :failExit
+configure.exe --debug --no-separate-debug-info --with-openssl-inc=\local\include --with-openssl-lib=\local\lib  || @echo failed to configure Psi+&goto :failExit
+
 goto :exit
 
 
 
 ::------------------------------------------------------------
-::--           FUNCTIONS SECTION
+::
+::           FUNCTIONS SECTION
+::
 ::------------------------------------------------------------
  ::  finds install path from windows registry. (any app from "Add/Remove software")
 :getInstallDirFromRegistryWOW64
 setlocal
+echo Search install dir for %1
 set root=HKLM
 if not %2xxx == xxx set root=%2
 set is64=0
@@ -113,31 +131,6 @@ goto :eof
 
 
 
-
-
-:fetchSvnList        - fetches set of something from svn
-setlocal
-set name=%~1
-set remote=%~2
-set items= %~3
-set subdir=%~4
-cd "%WORKDIR%"
-if "%remote%" == "" @echo invalid remote&goto :fetchSvnListFailed
-if not exist "%remote%" md "%remote%"
-cd "%remote%"
-for %%i in (%items%) do  (
-	@echo Fetch %%i %name%
-	if exist %%i ( "%SVN%" up %%i ) else "%SVN%" co %PSIPLUS_SVN%/%remote%/%%i/%subdir% %%i
-	if %ERRORLEVEL% neq 0 goto :fetchSvnListFailed
-)
-endlocal
-goto :eof
-
-:fetchSvnListFailed
-(	endlocal
-	set ERRORLEVEL=1
-)
-goto :eof
 
 
 
