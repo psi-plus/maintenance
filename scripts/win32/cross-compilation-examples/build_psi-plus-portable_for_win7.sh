c4ed9fbc9933b3bc75c58962de4119fbf5a31c4b
Boris Pek <tehnick-8@yandex.ru>
2017-07-23 01:52:20 +0300

    Update script for Psi+ cross compilation.

--- scripts/win32/cross-compilation-examples/build_psi-plus-portable_for_win7.sh
+++ scripts/win32/cross-compilation-examples/build_psi-plus-portable_for_win7.sh
@@ -3,11 +3,11 @@
 # Author:  Boris Pek <tehnick-8@yandex.ru>
 # License: MIT (Expat)
 # Created: 2017-07-14
-# Updated: 2017-07-17
+# Updated: 2017-07-23
 # Version: N/A
 #
 # Dependencies:
-# git, wget, curl, rsync, find, sed, zip
+# git, wget, curl, rsync, find, sed, p7zip
 # Sibuserv: https://github.com/sibuserv/sibuserv
 # MXE: https://github.com/sibuserv/mxe/tree/hobby
 
@@ -21,6 +21,7 @@ export SUFFIX="win7"
 PROGRAM_NAME="psi-plus"
 PROJECT_DIR_NAME="psi-plus-snapshots"
 TRANSLATIONS_DIR_NAME="psi-plus-l10n"
+QT_TRANSLATIONS_DIR_NAME="qt-l10n"
 DICTIONARIES_DIR_NAME="myspell"
 README_FILE_NAME="README.txt"
 
@@ -28,6 +29,8 @@ PROJECT_URL=https://github.com/psi-plus/psi-plus-snapshots.git
 TRANSLATIONS_URL=https://github.com/psi-plus/psi-plus-l10n.git
 DICTIONARIES_URL=https://deb.debian.org/debian/pool/main/libr/libreoffice-dictionaries
 README_URL=https://sourceforge.net/projects/psiplus/files/MS-Windows/Personal-Builds/tehnick
+
+ARCHIVER_OPTIONS="a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on"
 
 # Test Internet connection:
 host github.com > /dev/null
@@ -70,6 +73,31 @@ else
     echo "Creating ${MAIN_DIR}/${MOD}"
     cd "${MAIN_DIR}"
     git clone "${URL}"
+    echo;
+fi
+
+MOD="${QT_TRANSLATIONS_DIR_NAME}"
+if [ ! -d "${MAIN_DIR}/${MOD}" ]; then
+    echo "Getting Qt translations..."
+    # Load sibuserv settings
+    for CONF_FILE in "/etc/sibuserv/sibuserv.conf" \
+                     "${HOME}/.config/sibuserv/sibuserv.conf"
+    do
+        [ -r "${CONF_FILE}" ] && . "${CONF_FILE}"
+    done
+    # Copy translations from Qt SDK if it is available
+    if [ -d "${QT_SDK_DIR}/gcc_64/translations" ]; then
+        mkdir -p "${MAIN_DIR}/${MOD}"
+        cd "${MAIN_DIR}/${MOD}"
+        rsync -a "${QT_SDK_DIR}/gcc_64/translations"/qt*.qm ./
+        rm -f qt_help_*.qm qtconfig_*.qm qtconnectivity_*.qm qtlocation_*.qm \
+              qtquick1_*.qm qtquickcontrols_*.qm qtquickcontrols2_*.qm \
+              qtscript_*.qm qtserialport_*.qm qtwebengine_*.qm qtwebsockets_*.qm \
+              qtxmlpatterns_*.qm
+        echo "Done."
+    else
+        echo "Localization files are not found!"
+    fi
     echo;
 fi
 
@@ -139,6 +167,7 @@ for DIR in i686-w64-mingw32.shared x86_64-w64-mingw32.shared ; do
     cd   "${MAIN_DIR}/build-${PROJECT_DIR_NAME}/${DIR}"
     make prepare-bin-libs > /dev/null
     make prepare-bin      > /dev/null
+    cp -af "${MAIN_DIR}/${QT_TRANSLATIONS_DIR_NAME}"/*.qm psi/translations/ > /dev/null
     cp -af "${MAIN_DIR}/${PROJECT_DIR_NAME}/skins"  psi/ > /dev/null
     cp -af "${MAIN_DIR}/${PROJECT_DIR_NAME}/themes" psi/ > /dev/null
     cp -af "${MAIN_DIR}/${DICTIONARIES_DIR_NAME}"    psi/ > /dev/null
@@ -156,12 +185,15 @@ rsync -a --del "${MAIN_DIR}/build-${PROJECT_DIR_NAME}/x86_64-w64-mingw32.shared/
                "${PROGRAM_NAME}-${VERSION}_${SUFFIX}_x86_64/" > /dev/null
 echo;
 
-echo "Compressing files into zip archives..."
-rm -f ${PROGRAM_NAME}-${VERSION}_${SUFFIX}_x86*.zip
-zip -rq9 "${PROGRAM_NAME}-${VERSION}_${SUFFIX}_x86.zip" \
-         "${PROGRAM_NAME}-${VERSION}_${SUFFIX}_x86"
-zip -rq9 "${PROGRAM_NAME}-${VERSION}_${SUFFIX}_x86_64.zip" \
-         "${PROGRAM_NAME}-${VERSION}_${SUFFIX}_x86_64"
+echo "Compressing files into 7z archives..."
+rm -f ${PROGRAM_NAME}-${VERSION}_${SUFFIX}_x86*.7z
+echo "Creating archive: ${PROGRAM_NAME}-${VERSION}_${SUFFIX}_x86.7z"
+7z ${ARCHIVER_OPTIONS} "${PROGRAM_NAME}-${VERSION}_${SUFFIX}_x86.7z" \
+                       "${PROGRAM_NAME}-${VERSION}_${SUFFIX}_x86" > /dev/null
+echo "Creating archive: ${PROGRAM_NAME}-${VERSION}_${SUFFIX}_x86_64.7z"
+7z ${ARCHIVER_OPTIONS} "${PROGRAM_NAME}-${VERSION}_${SUFFIX}_x86_64.7z" \
+                       "${PROGRAM_NAME}-${VERSION}_${SUFFIX}_x86_64" > /dev/null
+echo "Done."
 echo;
 
 echo "Builds are ready for distribution and usage!"
