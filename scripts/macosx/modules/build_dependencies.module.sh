dcb6e47377fd84abc1746078bc7d711e09799536
Stanislav N. aka pztrn <pztrn@pztrn.name>
2018-06-16 15:45:47 +0500

    Dependencies, library bundling, version generation.
    Added missing dependencies building.
    
    Fixed libraries bundling. Probably this script will work only with
    Qt 5.11 and higher.
    
    Switched to unified version generation script.

--- scripts/macosx/modules/build_dependencies.module.sh
+++ scripts/macosx/modules/build_dependencies.module.sh
@@ -1,12 +1,16 @@
+DEP_GETTEXT_VER="0.19.8.1"
+DEP_GETTEXT_FILE="gettext-${DEP_GETTEXT_VER}.tar.gz"
+DEP_GETTEXT_URL="ftp://ftp.gnu.org/pub/gnu/gettext/${DEP_GETTEXT_FILE}"
+
 DEP_GROWL_URL="https://drive.google.com/uc?export=download&id=0B9THQ10qg_RSNWhuOW1rbV9WYmc"
 DEP_GROWL_FILE="Growl-2.0.1.tar.bz2"
 
-DEP_LIBGCRYPT_VER=1.8.2
+DEP_LIBGCRYPT_VER=1.8.3
 DEP_LIBGCRYPT_DIR=libgcrypt-${DEP_LIBGCRYPT_VER}
 DEP_LIBGCRYPT_FILE=${DEP_LIBGCRYPT_DIR}.tar.gz
-DEP_LIBGCRYPT_URL="ftp://ftp.gnupg.org/gcrypt/libgcrypt/${DEP_LIBGCRYPT_FILE}"
+DEP_LIBGCRYPT_URL="https://gnupg.org/ftp/gcrypt/libgcrypt/${DEP_LIBGCRYPT_FILE}"
 
-DEP_LIBGPGERROR_VER=1.27
+DEP_LIBGPGERROR_VER=1.31
 DEP_LIBGPGERROR_DIR=libgpg-error-${DEP_LIBGPGERROR_VER}
 DEP_LIBGPGERROR_FILE=${DEP_LIBGPGERROR_DIR}.tar.gz
 DEP_LIBGPGERROR_URL="ftp://ftp.gnupg.org/gcrypt/libgpg-error/${DEP_LIBGPGERROR_FILE}"
@@ -16,17 +20,27 @@ DEP_LIBOTR_DIR=libotr-${DEP_LIBOTR_VER}
 DEP_LIBOTR_FILE=${DEP_LIBOTR_DIR}.tar.gz
 DEP_LIBOTR_URL="https://otr.cypherpunks.ca/${DEP_LIBOTR_FILE}"
 
-DEP_LIBIDN_VER=1.33
+DEP_LIBIDN_VER=1.35
 DEP_LIBIDN_DIR=libidn-${DEP_LIBIDN_VER}
 DEP_LIBIDN_FILE=${DEP_LIBIDN_DIR}.tar.gz
-DEP_LIBIDN_URL="ftp://ftp.gnu.org/gnu/libidn/${DEP_LIBIDN_FILE}"
+DEP_LIBIDN_URL="https://ftp.gnu.org/gnu/libidn/${DEP_LIBIDN_FILE}"
 
-DEP_OPENSSL_VER=1.0.2n
+DEP_LIBSIGNAL_VER=2.3.2
+DEP_LIBSIGNAL_DIR=libsignal-protocol-c-${DEP_LIBSIGNAL_VER}
+DEP_LIBSIGNAL_FILE=v${DEP_LIBSIGNAL_VER}.tar.gz
+DEP_LIBSIGNAL_URL="https://github.com/signalapp/libsignal-protocol-c/archive/${DEP_LIBSIGNAL_FILE}"
+
+DEP_OPENSSL_VER=1.1.0h
 DEP_OPENSSL_DIR=openssl-${DEP_OPENSSL_VER}
 DEP_OPENSSL_FILE=${DEP_OPENSSL_DIR}.tar.gz
 DEP_OPENSSL_URL=https://ftp.openssl.org/source/${DEP_OPENSSL_FILE}
 
 DEP_QJDNS_GIT=https://github.com/psi-im/jdns.git
+
+DEP_QTKEYCHAIN_VER=0.8.0
+DEP_QTKEYCHAIN_DIR="qtkeychain-${DEP_QTKEYCHAIN_VER}"
+DEP_QTKEYCHAIN_FILE="v${DEP_QTKEYCHAIN_VER}.tar.gz"
+DEP_QTKEYCHAIN_URL="https://github.com/frankosterfeld/qtkeychain/archive/${DEP_QTKEYCHAIN_FILE}"
 
 DEP_ZLIB_VER=1.2.11
 DEP_ZLIB_DIR=zlib-${DEP_ZLIB_VER}
@@ -36,14 +50,17 @@ DEP_ZLIB_URL="http://zlib.net/${DEP_ZLIB_FILE}"
 function build_dependencies() {
     log "Building dependencies..."
     build_openssl
+    _build_dep_default "gettext" "${DEP_GETTEXT_VER}" "${DEP_GETTEXT_FILE}" "${DEP_GETTEXT_URL}" "libintl.8.dylib"
     _build_dep_default "libgpg-error" "${DEP_LIBGPGERROR_VER}" "${DEP_LIBGPGERROR_FILE}" "${DEP_LIBGPGERROR_URL}" "libgpg-error.0.dylib"
     _build_dep_default "libgcrypt" "${DEP_LIBGCRYPT_VER}" "${DEP_LIBGCRYPT_FILE}" "${DEP_LIBGCRYPT_URL}" "libgcrypt.20.dylib"
-    _build_dep_default "libidn" "${DEP_LIBIDN_VER}" "${DEP_LIBIDN_FILE}" "${DEP_LIBIDN_URL}" "libidn.11.dylib"
+    _build_dep_default "libidn" "${DEP_LIBIDN_VER}" "${DEP_LIBIDN_FILE}" "${DEP_LIBIDN_URL}" "libidn.12.dylib"
     _build_dep_default "zlib" "${DEP_ZLIB_VER}" "${DEP_ZLIB_FILE}" "${DEP_ZLIB_URL}" "libz.${DEP_ZLIB_VER}.dylib"
     build_minizip
     build_libotr
+    build_libsignal
     build_qjdns
     build_qca
+    build_qtkeychain
     build_growl
 }
 
@@ -97,6 +114,47 @@ function build_libotr() {
     ${MAKE} install > "${PSI_DIR}/logs/deps/libotr-makeinstall.log" 2>&1
     if [ $? -ne 0 ]; then
         action_failed "Installing libotr" "${PSI_DIR}/logs/deps/libotr-makeinstall.log"
+    fi
+}
+
+function build_libsignal() {
+    log "Installing dependency: libsignal-protocol-c..."
+
+    if [ -f "${DEPS_ROOT}/lib/libsignal-protocol-c.a" ]; then
+        log "libsignal-protocol-c already built, skipping..."
+        return
+    fi
+
+    if [ ! -d "${DEPS_BUILDROOT}/libsignal-protocol-c" ]; then
+        log "libsignal-protocol-c sources wasn't found, fetching..."
+        mkdir -p "${DEPS_BUILDROOT}/libsignal-protocol-c"
+        cd "${DEPS_BUILDROOT}/libsignal-protocol-c"
+        curl -L -o "${DEP_LIBSIGNAL_FILE}" "${DEP_LIBSIGNAL_URL}"
+        tar -xf "${DEP_LIBSIGNAL_FILE}"
+    fi
+
+    if [ -d "${DEPS_BUILDROOT}/libsignal-protocol-c/${DEP_LIBSIGNAL_DIR}/build" ]; then
+        rm -rf "${DEPS_BUILDROOT}/libsignal-protocol-c/${DEP_LIBSIGNAL_DIR}/build"
+    fi
+
+    mkdir -p "${DEPS_BUILDROOT}/libsignal-protocol-c/${DEP_LIBSIGNAL_DIR}/build"
+    cd "${DEPS_BUILDROOT}/libsignal-protocol-c/${DEP_LIBSIGNAL_DIR}/build"
+    local cmakeopts="-DCMAKE_INSTALL_PREFIX=${DEPS_ROOT} -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_BUILD_TYPE=Release"
+    ${CMAKE} ${cmakeopts} .. > "${PSI_DIR}/logs/deps/libsignal-protocol-c-cmake.log"
+    if [ $? -ne 0 ]; then
+        action_failed "Configuring libsignal-protocol-c" "${PSI_DIR}/logs/deps/libsignal-protocol-c-cmake.log"
+    fi
+
+    log "Building..."
+    ${MAKE} ${MAKEOPTS} > "${PSI_DIR}/logs/deps/libsignal-protocol-c-make.log" 2>&1
+    if [ $? -ne 0 ]; then
+        action_failed "Building libsignal-protocol-c" "${PSI_DIR}/logs/deps/libsignal-protocol-c-make.log"
+    fi
+
+    log "Installing..."
+    ${MAKE} install > "${PSI_DIR}/logs/deps/libsignal-protocol-c-makeinstall.log" 2>&1
+    if [ $? -ne 0 ]; then
+        action_failed "Installing libsignal-protocol-c" "${PSI_DIR}/logs/deps/libsignal-protocol-c-makeinstall.log"
     fi
 }
 
@@ -206,7 +264,7 @@ function build_qca() {
     log "Installing..."
     ${MAKE} install > "${PSI_DIR}/logs/deps/qca-makeinstall.log" 2>&1
     if [ $? -ne 0 ]; then
-        action_failed "Building QCA" "${PSI_DIR}/logs/deps/qca-make.log"
+        action_failed "Installing QCA" "${PSI_DIR}/logs/deps/qca-makeinstall.log"
     fi
 }
 
@@ -247,9 +305,50 @@ function build_qjdns() {
     log "Installing..."
     ${MAKE} install > "${PSI_DIR}/logs/deps/qjdns-makeinstall.log" 2>&1
     if [ $? -ne 0 ]; then
-        action_failed "Building QJDNS" "${PSI_DIR}/logs/deps/qjdns-make.log"
+        action_failed "Installing QJDNS" "${PSI_DIR}/logs/deps/qjdns-makeinstall.log"
     fi
 
+}
+
+function build_qtkeychain() {
+    log "Installing dependency: qtkeychain..."
+
+    if [ -f "${DEPS_ROOT}/lib/libqt5keychain.1.dylib" ]; then
+        log "QtKeychain already built, skipping..."
+        return
+    fi
+
+    if [ ! -d "${DEPS_BUILDROOT}/qtkeychain" ]; then
+        log "qtkeychain sources wasn't found, fetching..."
+        mkdir -p "${DEPS_BUILDROOT}/qtkeychain"
+        cd "${DEPS_BUILDROOT}/qtkeychain"
+        curl -L -o "${DEP_QTKEYCHAIN_FILE}" "${DEP_QTKEYCHAIN_URL}"
+        tar -xf "${DEP_QTKEYCHAIN_FILE}"
+    fi
+
+    if [ -d "${DEPS_BUILDROOT}/qtkeychain/${DEP_QTKEYCHAIN_DIR}/build" ]; then
+        rm -rf "${DEPS_BUILDROOT}/qtkeychain/${DEP_QTKEYCHAIN_DIR}/build"
+    fi
+
+    mkdir -p "${DEPS_BUILDROOT}/qtkeychain/${DEP_QTKEYCHAIN_DIR}/build"
+    cd "${DEPS_BUILDROOT}/qtkeychain/${DEP_QTKEYCHAIN_DIR}/build"
+    local cmakeopts="-DCMAKE_INSTALL_PREFIX=${DEPS_ROOT} -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_BUILD_TYPE=Release -DECM_MKSPECS_INSTALL_DIR=${DEPS_ROOT}/lib/mkspecs/modules"
+    ${CMAKE} ${cmakeopts} .. > "${PSI_DIR}/logs/deps/qtkeychain-cmake.log"
+    if [ $? -ne 0 ]; then
+        action_failed "Configuring QtKeychain" "${PSI_DIR}/logs/deps/qtkeychain-cmake.log"
+    fi
+
+    log "Building..."
+    ${MAKE} ${MAKEOPTS} > "${PSI_DIR}/logs/deps/qtkeychain-make.log" 2>&1
+    if [ $? -ne 0 ]; then
+        action_failed "Building QtKeychain" "${PSI_DIR}/logs/deps/qtkeychain-make.log"
+    fi
+
+    log "Installing..."
+    ${MAKE} install > "${PSI_DIR}/logs/deps/qtkeychain-makeinstall.log" 2>&1
+    if [ $? -ne 0 ]; then
+        action_failed "Installing QtKeychain" "${PSI_DIR}/logs/deps/qtkeychain-makeinstall.log"
+    fi
 }
 
 function _build_dep_default() {
@@ -267,7 +366,7 @@ function _build_dep_default() {
     fi
 
     if [ ! -d "${DEPS_BUILDROOT}/${depname}" ]; then
-        log "${depname} sources wasn't found, fetching..."
+        log "${depname} sources wasn't found, fetching from '${depurl}'..."
         mkdir -p "${DEPS_BUILDROOT}/${depname}"
         cd "${DEPS_BUILDROOT}/${depname}"
         curl -L -o "${depfile}" "${depurl}"
--- scripts/macosx/modules/create_version_string.module.sh
+++ scripts/macosx/modules/create_version_string.module.sh
@@ -6,41 +6,60 @@ PATCHES_VERSION_COMMIT=db1a4053ef7aae72ae819eb00eba47bae9530320
 # DMG file.
 #####################################################################
 
+#function create_version_string() {
+#    # Create version string.
+#    log "Creating version string for about dialog..."
+#    # Snapshotted thing already have everything for version string.
+#    PSI_REVISIONS_FROM_VERSION=`cd "${PSI_SOURCE_DIR}/admin" && ./git_revnumber.sh`
+#    PLUGINS_REVISIONS_FROM_VERSION=`cd "${PSI_DIR}/plus" && git rev-list --count ${PATCHES_VERSION_COMMIT}..HEAD`
+#    PSI_REVISION=`cd "${PSI_SOURCE_DIR}" && git rev-parse --short HEAD`
+#    PSI_PLUS_REVISION=`cd "${PSI_DIR}/plus" && git rev-parse --short HEAD`
+#    PSI_BUILD=$[ ${PSI_REVISIONS_FROM_VERSION} + ${PLUGINS_REVISIONS_FROM_VERSION} ]
+#    VERSION_STRING_RAW="${PSI_VERSION}.${PSI_BUILD}"
+#
+#    if [ ${BUILD_PSI_PLUS} -eq 1 ]; then
+#        VERSION_STRING="${VERSION_STRING_RAW} ($(date +"%Y-%m-%d"), Psi:${PSI_REVISION}, Psi+:${PSI_PLUS_REVISION}"
+#    else
+#        VERSION_STRING="${VERSION_STRING_RAW} ($(date +"%Y-%m-%d"), Psi:${PSI_REVISION}"
+#    fi
+#
+#    if [ "${ENABLE_WEBENGINE}" -eq "1" ]; then
+#        VERSION_STRING="${VERSION_STRING}, webengine)"
+#    else
+#        VERSION_STRING="${VERSION_STRING})"
+#    fi
+#
+#    # Add qt5 note to VERSION_STRING_RAW as we support only it on macs.
+#    # Should be removed at version 2.0 :).
+#    # Also add note about webengine usage.
+#    if [ "${ENABLE_WEBENGINE}" -eq "1" ]; then
+#        VERSION_STRING_RAW="${VERSION_STRING_RAW}-qt5-webengine"
+#    else
+#        VERSION_STRING_RAW="${VERSION_STRING_RAW}-qt5"
+#    fi
+#
+#    log "Version string: ${VERSION_STRING}"
+#    log "Raw version string (will be used e.g. in filename): ${VERSION_STRING_RAW}"
+#
+#    # Just a build date.
+#    BUILD_DATE=`date +'%Y-%m-%d'`
+#}
+
 function create_version_string() {
-    # Create version string.
-    log "Creating version string for about dialog..."
-    # Snapshotted thing already have everything for version string.
-    PSI_REVISIONS_FROM_VERSION=`cd "${PSI_SOURCE_DIR}/admin" && ./git_revnumber.sh`
-    PLUGINS_REVISIONS_FROM_VERSION=`cd "${PSI_DIR}/plus" && git rev-list --count ${PATCHES_VERSION_COMMIT}..HEAD`
-    PSI_REVISION=`cd "${PSI_SOURCE_DIR}" && git rev-parse --short HEAD`
-    PSI_PLUS_REVISION=`cd "${PSI_DIR}/plus" && git rev-parse --short HEAD`
-    PSI_BUILD=$[ ${PSI_REVISIONS_FROM_VERSION} + ${PLUGINS_REVISIONS_FROM_VERSION} ]
-    VERSION_STRING_RAW="${PSI_VERSION}.${PSI_BUILD}"
+    log "Creating version string..."
 
-    if [ ${BUILD_PSI_PLUS} -eq 1 ]; then
-        VERSION_STRING="${VERSION_STRING_RAW} ($(date +"%Y-%m-%d"), Psi:${PSI_REVISION}, Psi+:${PSI_PLUS_REVISION}"
-    else
-        VERSION_STRING="${VERSION_STRING_RAW} ($(date +"%Y-%m-%d"), Psi:${PSI_REVISION}"
+    cd "${PSI_DIR}"
+    local version_cmd="plus/admin/psi-plus-nightly-version build"
+    if [ "${ENABLE_WEBENGINE}" == "1" ]; then
+        version_cmd="${version_cmd} --webengine"
     fi
 
-    if [ "${ENABLE_WEBENGINE}" -eq "1" ]; then
-        VERSION_STRING="${VERSION_STRING}, webengine)"
-    else
-        VERSION_STRING="${VERSION_STRING})"
-    fi
-
-    # Add qt5 note to VERSION_STRING_RAW as we support only it on macs.
-    # Should be removed at version 2.0 :).
-    # Also add note about webengine usage.
-    if [ "${ENABLE_WEBENGINE}" -eq "1" ]; then
-        VERSION_STRING_RAW="${VERSION_STRING_RAW}-qt5-webengine"
-    else
-        VERSION_STRING_RAW="${VERSION_STRING_RAW}-qt5"
+    VERSION_STRING=$(${version_cmd})
+    VERSION_STRING_RAW=$(echo ${VERSION_STRING} | cut -d " " -f 1)
+    if [ "${ENABLE_WEBENGINE}" == "1" ]; then
+        VERSION_STRING_RAW="${VERSION_STRING_RAW}-webengine"
     fi
 
     log "Version string: ${VERSION_STRING}"
-    log "Raw version string (will be used e.g. in filename): ${VERSION_STRING_RAW}"
-
-    # Just a build date.
-    BUILD_DATE=`date +'%Y-%m-%d'`
+    log "Version string for filename: ${VERSION_STRING_RAW}"
 }
