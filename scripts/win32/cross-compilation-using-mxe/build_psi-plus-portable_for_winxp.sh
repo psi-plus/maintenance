bbee9316591ae511833a01150a7aba65b5c59e38
Boris Pek <tehnick-8@yandex.ru>
2019-02-26 01:20:26 +0300

    Update scripts for Psi+ cross-compilation for Win7 and WinXP.

--- scripts/win32/cross-compilation-using-mxe/build_psi-plus-portable_for_winxp.sh
+++ scripts/win32/cross-compilation-using-mxe/build_psi-plus-portable_for_winxp.sh
@@ -3,7 +3,7 @@
 # Author:  Boris Pek <tehnick-8@yandex.ru>
 # License: MIT (Expat)
 # Created: 2017-07-14
-# Updated: 2018-11-24
+# Updated: 2019-02-26
 # Version: N/A
 #
 # Dependencies:
@@ -27,9 +27,11 @@ README_FILE_NAME="README.txt"
 
 PROJECT_URL=https://github.com/psi-plus/psi-plus-snapshots.git
 TRANSLATIONS_URL=https://github.com/psi-plus/psi-plus-l10n.git
+QT_TRANSLATIONS_URL=/usr/share/qt5/translations
 DICTIONARIES_URL=https://deb.debian.org/debian/pool/main/libr/libreoffice-dictionaries
 README_URL=https://sourceforge.net/projects/psiplus/files/Windows/Personal-Builds/tehnick
 
+BUILD_TARGETS="i686-w64-mingw32.shared"
 ARCHIVER_OPTIONS="a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on"
 
 # Test Internet connection:
@@ -82,21 +84,11 @@ fi
 MOD="${QT_TRANSLATIONS_DIR_NAME}"
 if [ ! -d "${MAIN_DIR}/${MOD}" ]; then
     echo "Getting Qt translations..."
-    # Load sibuserv settings
-    for CONF_FILE in "/etc/sibuserv/sibuserv.conf" \
-                     "${HOME}/.config/sibuserv/sibuserv.conf"
-    do
-        [ -r "${CONF_FILE}" ] && . "${CONF_FILE}"
-    done
-    # Copy translations from Qt SDK if it is available
-    if [ -d "${QT_SDK_DIR}/android_armv7/translations" ]; then
+    # Copy Qt translations from current system
+    if [ -d "${QT_TRANSLATIONS_URL}" ]; then
         mkdir -p "${MAIN_DIR}/${MOD}"
         cd "${MAIN_DIR}/${MOD}"
-        rsync -a "${QT_SDK_DIR}/android_armv7/translations"/qt*.qm ./
-        rm -f qt_help_*.qm qtconfig_*.qm qtconnectivity_*.qm qtlocation_*.qm \
-              qtquick1_*.qm qtquickcontrols_*.qm qtquickcontrols2_*.qm \
-              qtscript_*.qm qtserialport_*.qm qtwebengine_*.qm qtwebsockets_*.qm \
-              qtxmlpatterns_*.qm
+        rsync -a "${QT_TRANSLATIONS_URL}"/q*.qm ./
         echo "Done."
     else
         echo "Localization files are not found!"
@@ -137,8 +129,9 @@ fi
 
 cd "${MAIN_DIR}"
 echo "Preparing to build..."
-
 rsync -a --del "${MAIN_DIR}/${TRANSLATIONS_DIR_NAME}/translations" \
+               "${MAIN_DIR}/${PROJECT_DIR_NAME}/" > /dev/null
+rsync -a --del "${MAIN_DIR}/${DICTIONARIES_DIR_NAME}" \
                "${MAIN_DIR}/${PROJECT_DIR_NAME}/" > /dev/null
 
 cd "${MAIN_DIR}/${PROJECT_DIR_NAME}"
@@ -153,35 +146,35 @@ sed -i "s|option( BUILD_DEV_PLUGINS .*$|option( BUILD_DEV_PLUGINS \"\" ON )|g" s
 sed -i "s|option( USE_QJDNS .*$|option( USE_QJDNS \"\" ON )|g" CMakeLists.txt
 
 rm -rf "${MAIN_DIR}/build-${PROJECT_DIR_NAME}"
+echo "Done."
 echo;
 
 cd "${MAIN_DIR}/${PROJECT_DIR_NAME}"
 echo "Building basic version of Psi+ with plugins..."
-build-project i686-w64-mingw32.shared
+build-project ${BUILD_TARGETS}
 echo;
 
 cd "${MAIN_DIR}/build-${PROJECT_DIR_NAME}"
 echo "Preparing to the next step..."
 sed -i "s|ENABLE_PLUGINS:BOOL=.*$|ENABLE_PLUGINS:BOOL=OFF|g" */CMakeCache.txt
 sed -i "s|ENABLE_WEBKIT:BOOL=.*$|ENABLE_WEBKIT:BOOL=ON|g"    */CMakeCache.txt
+echo "Done."
 echo;
 
 cd "${MAIN_DIR}/${PROJECT_DIR_NAME}"
 echo "Building webkit version of Psi+ without plugins..."
-build-project i686-w64-mingw32.shared
+build-project ${BUILD_TARGETS}
 echo;
 
 cd "${MAIN_DIR}/build-${PROJECT_DIR_NAME}"
 echo "Copying libraries and resources to..."
-for DIR in i686-w64-mingw32.shared ; do
+for DIR in ${BUILD_TARGETS} ; do
     echo "${MAIN_DIR}/build-${PROJECT_DIR_NAME}/${DIR}/psi"
     cd   "${MAIN_DIR}/build-${PROJECT_DIR_NAME}/${DIR}"
     make prepare-bin-libs > /dev/null
     make prepare-bin      > /dev/null
     cp -af "${MAIN_DIR}/${QT_TRANSLATIONS_DIR_NAME}"/*.qm psi/translations/ > /dev/null
     cp -af "${MAIN_DIR}/${PROJECT_DIR_NAME}/skins"  psi/ > /dev/null
-    cp -af "${MAIN_DIR}/${PROJECT_DIR_NAME}/themes" psi/ > /dev/null
-    cp -af "${MAIN_DIR}/${DICTIONARIES_DIR_NAME}"   psi/ > /dev/null
     cp -af "${MAIN_DIR}/README.txt" psi/ > /dev/null
 done
 echo;
@@ -191,6 +184,7 @@ echo "Copying the results to main directory..."
 mkdir -p "${ARCHIVE_DIR_NAME}"
 rsync -a --del "${MAIN_DIR}/build-${PROJECT_DIR_NAME}/i686-w64-mingw32.shared/psi/" \
                "${ARCHIVE_DIR_NAME}/" > /dev/null
+echo "Done."
 echo;
 
 echo "Compressing files into 7z archives..."
